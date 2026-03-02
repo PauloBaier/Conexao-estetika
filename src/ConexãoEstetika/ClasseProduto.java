222f339060a1eee78ab3fe36786869b8b1dca756
Matheus Fritsche <matheusvfritsche@gmail.com>
2026-03-02 19:13:53 -0300

    ClasseProduto

---
+++ src/ConexãoEstetika/ClasseProduto.java
@@ -0,0 +1,115 @@
+package ConexãoEstetika;
+
+public class Produto {  //atributos
+    private String id;
+    private String nome;
+    private String descricao;
+    private double precoCusto;
+    private double precoVenda;
+    private int quantidadeEstoque;
+    private String marca;
+
+    Fornecedor fornecedor;
+
+    //constructor
+    public Produto(String id, String nome, String descricao, double precoCusto, double precoVenda, int quantidadeEstoque, String marca, Fornecedor fornecedor) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.precoCusto = precoCusto;
+        this.precoVenda = precoVenda;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.marca = marca;
+        this.fornecedor = fornecedor;
+    }
+
+    //getters e setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+
+    //metodos
+
+    //adicionar estoque
+    public void adicionarEstoque(int quantidade) {
+        if (quantidade > 0) {
+            this.quantidadeEstoque += quantidade;
+        }
+    }
+
+    //remover estoque
+    public boolean removerEstoque(int quantidade) {
+        if (quantidade > 0 && quantidade <= this.quantidadeEstoque) {
+            this.quantidadeEstoque -= quantidade;
+            return true;
+        }
+        return false;
+    }
+
+    //Calcular margem de lucro
+    public double calcularMargemLucro() {
+        if (precoCusto == 0) return 0;
+        return ((precoVenda - precoCusto) / precoCusto) * 100;
+    }
+}
