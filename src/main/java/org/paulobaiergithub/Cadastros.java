b5dffc81c11d59f92d4c4fc03d588c00ce91d793
Paulo Baier <baierpaulo312@gmail.com>
2026-03-02 19:26:53 -0300

    Classe Cadastro feito

---
+++ src/main/java/org/paulobaiergithub/Cadastros.java
@@ -0,0 +1,34 @@
+package org.paulobaiergithub;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Cadastros <T extends IIdentificador>{
+    List<T> listaCadastros = new ArrayList<>();
+
+    public void adicionar(T cadastro){
+        this.listaCadastros.add(cadastro);
+    }
+
+    public void remover(int id){
+        Optional<T> resultado = this.listaCadastros.stream().filter(c -> c.getId() == id).findFirst();
+        if(resultado.isEmpty()){
+            throw new RuntimeException("ERRO: Cadastro não encontrado!");
+        }
+
+        this.listaCadastros.remove(resultado.get());
+    }
+
+    public T buscarId(int id){
+        Optional<T> resultado = this.listaCadastros.stream().filter(c -> c.getId() == id).findFirst();
+        if(resultado.isEmpty()){
+            throw new RuntimeException("ERRO: Cadastro não encontrado!");
+        }
+        return resultado.get();
+    }
+
+    public List<T> listarTodos(){
+        return this.listaCadastros;
+    }
+}
