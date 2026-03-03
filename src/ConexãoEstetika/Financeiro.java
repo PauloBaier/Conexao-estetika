692e5acaf817f5b5ac42aecad43dc11106ec2de5
Matheus Fritsche <matheusvfritsche@gmail.com>
2026-03-03 20:20:13 -0300

    Atualizando Financeiro

--- src/ConexãoEstetika/Financeiro.java
+++ src/ConexãoEstetika/Financeiro.java
@@ -1,40 +1,74 @@
 package ConexãoEstetika;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Financeiro {
     private List<ContaReceber> contasReceber;
     private List<ContaPagar> contasPagar;
-
-    private List<Movimentacao> movimentacoes; //registra (entrata/saida), valor, data, descrição, referencia (venda, conta, etc.)
 
 
     //construtor
     public Financeiro() {
         this.contasReceber = new ArrayList<>();
         this.contasPagar = new ArrayList<>();
-        this.movimentacoes = new ArrayList<>();
-
     }
 
-    //getters
+    //getter
     public List<ContaReceber> getContasReceber() {
         return Collections.unmodifiableList(contasReceber);
     }
-
-
+    public List<ContaPagar> getContasPagar() {
+        return Collections.unmodifiableList(contasPagar);
+    }
 
     //METODOS
     //contas a receber
-    public void adicionarContaReceber(ContaReceber conta)
-    public void receberConta(ContaReceber conta)  //marcar conta como paga e somar no saldo
+
+    public void adicionarContaReceber(ContaReceber conta) {
+        contasReceber.add(conta);
+    }
+
+    public boolean alterarStatusContaReceber(String id, boolean novoStatus) {
+        for (ContaReceber conta : contasReceber) {
+            if (conta.getId().equals(id)) {
+                conta.setPaga(novoStatus);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean removerContaReceber(String id) {
+        return contasReceber.removeIf(conta -> conta.getId().equals(id));
+    }
+
+    public List<ContaReceber> listarTodasContasReceber() {
+        return new ArrayList<>(contasReceber);
+    }
 
     //contas a pagar
-    public void adicionarContaPagar(ContaPagar conta)
-    public void pagarConta(ContaPagar conta) //marcar conta como paga e subtrair do saldo
+    public void adicionarContaPagar(ContaPagar conta) {
+        contasPagar.add(conta);
+    }
+
+    public boolean alterarStatusContaPagar(String id, boolean novoStatus) {
+        for (ContaPagar conta : contasPagar) {
+            if (conta.getId().equals(id)) {
+                conta.setPaga(novoStatus);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean removerContaPagar(String id) {
+        return contasPagar.removeIf(conta -> conta.getId().equals(id));
+    }
+
+    public List<ContaPagar> listarTodasContasPagar() {
+        return new ArrayList<>(contasPagar);
+    }
 
 
-    //movimentacoes
-    private void registrarMovimentacao(String tipo, double valor, String descricao)
-
-    public List<ContaReceber> listarContasReceberEmAberto()
-    public List<ContaPagar> listarContasPagarEmAberto()
-}
