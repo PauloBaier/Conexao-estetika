f860b24b4ff3f0396a971d7eedc41da0d427a3c0
Paulo Baier <baierpaulo312@gmail.com>
2026-03-03 19:20:00 -0300

    Ajustes

--- src/main/java/org/conexaoestetika/Main.java
+++ src/main/java/org/conexaoestetika/Main.java
@@ -1,10 +1,188 @@
 package org.conexaoestetika;
-
-
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        int opcao;
+
+        do {
+            System.out.println("\n===== MENU PRINCIPAL =====");
+            System.out.println("1 - Cadastros");
+            System.out.println("2 - Venda");
+            System.out.println("3 - Financeiro");
+            System.out.println("4 - Relatórios");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    menuCadastros();
+                    break;
+                case 2:
+                    menuVenda();
+                    break;
+                case 3:
+                    menuFinanceiro();
+                    break;
+                case 4:
+                    menuRelatorios();
+                    break;
+                case 0:
+                    System.out.println("Saindo do sistema...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+        } while (opcao != 0);
+    }
+
+    public static void menuCadastros() {
+        int opcao;
+
+        do {
+            System.out.println("\n===== MENU CADASTROS =====");
+            System.out.println("1 - Cadastrar Cliente");
+            System.out.println("2 - Cadastrar Fornecedor");
+            System.out.println("3 - Cadastrar Produto");
+            System.out.println("4 - Listar Clientes");
+            System.out.println("5 - Listar Fornecedores");
+            System.out.println("6 - Listar Produtos");
+            System.out.println("7 - Remover Cliente");
+            System.out.println("8 - Remover Fornecedor");
+            System.out.println("9 - Remover Produto");
+            System.out.println("0 - Voltar");
+
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Cadastrar Cliente...");
+                    break;
+                case 2:
+                    System.out.println("Cadastrar Fornecedor...");
+                    break;
+                case 3:
+                    System.out.println("Cadastrar Produto...");
+                    break;
+                case 4:
+                    System.out.println("Listar Clientes...");
+                    break;
+                case 5:
+                    System.out.println("Listar Fornecedores...");
+                    break;
+                case 6:
+                    System.out.println("Listar Produtos...");
+                    break;
+                case 7:
+                    System.out.println("Remover Cliente...");
+                    break;
+                case 8:
+                    System.out.println("Remover Fornecedor...");
+                    break;
+                case 9:
+                    System.out.println("Remover Produto...");
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+        } while (opcao != 0);
+    }
+
+    public static void menuVenda() {
+        int opcao;
+
+        do {
+            System.out.println("\n===== MENU VENDA =====");
+            System.out.println("1 - Nova Venda");
+            System.out.println("2 - Cancelar Venda");
+            System.out.println("0 - Voltar");
+
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Adicionar Cliente...");
+                    System.out.println("Adicionar Produtos...");
+                    System.out.println("Fechar Venda...");
+                    break;
+                case 2:
+                    System.out.println("Venda cancelada!");
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+        } while (opcao != 0);
+    }
+
+    public static void menuFinanceiro() {
+        int opcao;
+
+        do {
+            System.out.println("\n===== MENU FINANCEIRO =====");
+            System.out.println("1 - Contas a Receber");
+            System.out.println("2 - Contas a Pagar");
+            System.out.println("0 - Voltar");
+
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Marcar conta como paga...");
+                    break;
+                case 2:
+                    System.out.println("Cadastrar Conta a Pagar...");
+                    System.out.println("Marcar Conta como paga...");
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+        } while (opcao != 0);
+    }
+
+    public static void menuRelatorios() {
+        int opcao;
+
+        do {
+            System.out.println("\n===== MENU RELATÓRIOS =====");
+            System.out.println("1 - Contas a Receber / Vendas");
+            System.out.println("2 - Contas a Pagar");
+            System.out.println("3 - Produtos (Estoque)");
+            System.out.println("0 - Voltar");
+
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Filtrar por data e status...");
+                    break;
+                case 2:
+                    System.out.println("Filtrar por data e status...");
+                    break;
+                case 3:
+                    System.out.println("Filtro: Todos / Estoque Baixo...");
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+        } while (opcao != 0);
     }
 }
