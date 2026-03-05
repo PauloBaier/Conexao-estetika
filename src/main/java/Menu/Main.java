67512e79d8cb20a359c8604297ec30a92198c7c4
Cristiano Sgarbossa <cristianosgarbossadev@gmail.com>
2026-03-05 18:57:17 -0300

    AtualizacaoMenu

--- src/main/java/Menu/Main.java
+++ src/main/java/Menu/Main.java
@@ -58,36 +58,37 @@ public class Main {
             System.out.println("8 - Remover Fornecedor");
             System.out.println("9 - Remover Produto");
             System.out.println("0 - Voltar");
+            System.out.println("Escolha uma opcão: ");
 
             opcao = sc.nextInt();
 
             switch (opcao) {
                 case 1:
-                    System.out.println("Cadastrar Cliente...");
+                  cadastroCliente();
                     break;
                 case 2:
-                    System.out.println("Cadastrar Fornecedor...");
+                    cadastroFornecedor();
                     break;
                 case 3:
-                    System.out.println("Cadastrar Produto...");
+                    cadastroProduto();
                     break;
                 case 4:
-                    System.out.println("Listar Clientes...");
+                    listarCliente();
                     break;
                 case 5:
-                    System.out.println("Listar Fornecedores...");
+                    listarFornecedor();
                     break;
                 case 6:
-                    System.out.println("Listar Produtos...");
+                    listarProduto();
                     break;
                 case 7:
-                    System.out.println("Remover Cliente...");
+                    removerCliente();
                     break;
                 case 8:
-                    System.out.println("Remover Fornecedor...");
+                    removerFornecedor();
                     break;
                 case 9:
-                    System.out.println("Remover Produto...");
+                    removerProduto();
                     break;
                 case 0:
                     break;
@@ -184,5 +185,187 @@ public class Main {
             }
 
         } while (opcao != 0);
+    }
+
+    public void cadastroCliente(Sistema sistema) {
+
+        System.out.print("ID: " + id);
+
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Telefone: ");
+        String telefone = sc.nextLine();
+
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+
+        System.out.print("CPF (11 números): ");
+        String cpf = sc.nextLine();
+
+        System.out.print("Dinheiro Gasto: ");
+        double dinheiroGasto = sc.nextDouble();
+
+        sistema.novoCadastroCliente(nome, telefone, email, cpf, dinheiroGasto);
+
+        System.out.println("Cliente cadastro com sucesso!");
+    }
+
+    public void cadastroFornecedor(Sistema sistema) {
+
+        System.out.print("ID: " + id);
+
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Telefone: ");
+        String telefone = sc.nextLine();
+
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+
+        System.out.print("CNPJ(14 números): ");
+        String cnpj = sc.nextLine();
+
+        System.out.print("Razão Social: ");
+        String razaoSocial = sc.nextLine();
+
+        sistema.novoCadastroFornecedor(nome, telefone, email, cnpj, razaoSocial);
+
+        System.out.println("Fornecedor cadastrado com sucesso!");
+    }
+
+    public void cadastroProduto(Sistema sistema) {
+
+        System.out.print("ID: " + id);
+
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Descrição: ");
+        String descricao = sc.nextLine();
+
+        System.out.print("Preço de Custo: ");
+        double precoCusto = sc.nextDouble();
+
+        System.out.print("Preço de Venda: ");
+        double precoVenda = sc.nextDouble();
+
+        System.out.print("Quantidade de Estoque: ");
+        int qntdEstoque = sc.nextInt();
+
+        System.out.print("Estoque Minimo: ");
+        int estoqueMinimo = sc.nextInt();
+
+        System.out.println("Fornecedor: ");
+        Fornecedor fornecedor = escolherFornecedor();
+
+        sistema.novoCadastroProduto(nome, descricao, precoCusto, precoVenda, qntdEstoque, estoqueMinimo, fornecedor);
+
+        System.out.println("Pproduto cadastrado com sucesso!");
+
+    }
+
+    public Fornecedor escolherFornecedor() {
+
+        for (Fornecedor f : listaFornecedores()) {
+            System.out.println("ID: " + f.getId() + " | Nome: " + f.getNome());
+        }
+
+        System.out.print("Digite o ID do fornecedor: ");
+        int idEscolhido = sc.nextInt();
+
+        for (Fornecedor f : listaFornecedores()) {
+            if (f.getId() == idEscolhido) {
+                return f;
+            }
+        }
+
+        System.out.println("Fornecedor não encontrado.");
+        return null;
+    }
+
+    public void listarCliente() {
+
+        for(Cliente c : listaClientes()) {
+            System.out.println("ID: " + c.getId()
+                    + "| Nome: " + c.getNome()
+                    + "| Telefone" + c.getTelefone()
+                    + "| Email: " + c.getEmail()
+                    + "| CPF: " + c.getCpf()
+                    + "| Dinheiro Gasto: " + c.getDinheiroGasto());
+        }
+    }
+
+    public void listarFornecedor() {
+
+        for(Fornecedor f : listaFornecedores()) {
+            System.out.println("ID: " + f.getId()
+                    + "| Nome: " + f.getNome()
+                    + "| Telefone" + f.getTelefone()
+                    + "| Email: " + f.getEmail()
+                    + "| CNPJ: " + f.getCnpj()
+                    + "| Razão Social: " + f.getRazaoSocial());
+        }
+    }
+
+    public void listarProduto() {
+        for(Produto p : listaProdutos()) {
+            System.out.println("ID: " + p.getId()
+                    + "| Nome: " + p.getNome()
+                    + "| Descrição" + p.getDescricao()
+                    + "| Preço de Custo: " + p.getPrecoCusto()
+                    + "| Preço de Venda: " + p.getPrecoVenda()
+                    + "| Quantidade Estoque: " + p.getQuantidadeEstoque
+                    + "| Estoque Minimo: " + p.getEstoqueMinimo());
+        }
+    }
+
+    public void removerCliente(Sistema sistema) {
+
+        listarCliente();
+
+        System.out.print("Digite o ID do cliente que deseja remover: ");
+        int id = sc.nextInt();
+
+        boolean removido = sistema.removerCliente(id);
+
+        if(removido){
+            System.out.println("Cliente removido com sucesso!");
+        } else {
+            System.out.println("Cliente não encontrado.");
+        }
+    }
+
+    public void removerFornecedor(Sistema sistema) {
+
+        listarFornecedor();
+
+        System.out.print("Digite o ID do fornecedor que deseja remover: ");
+        int id = sc.nextInt();
+
+        boolean removido = sistema.removerFornecedor(id);
+
+        if(removido){
+            System.out.println("Fornecedor removido com sucesso!");
+        } else {
+            System.out.println("Fornecedor não encontrado.");
+        }
+    }
+
+    public void removerProduto(Sistema sistema) {
+
+        listarProduto();
+
+        System.out.print("Digite o ID do produto que deseja remover: ");
+        int id = sc.nextInt();
+
+        boolean removido = sistema.removerProduto(id);
+
+        if(removido){
+            System.out.println("Produto removido com sucesso!");
+        } else {
+            System.out.println("Produto não encontrado.");
+        }
     }
 }
