dae18b8c302133e5d7fa86b5eece7a766e9954f9
Cristiano Sgarbossa <cristianosgarbossadev@gmail.com>
2026-04-10 21:18:08 -0300

    Delete src/main directory

--- src/main/java/conexao/IIdentificador.java
+++
@@ -1,6 +0,0 @@
-package conexao;
-
-public interface IIdentificador {
-    int getId();
-    void setId(int id);
-}
--- src/main/java/model/enums/StatusCaixa.java
+++
@@ -1,6 +0,0 @@
-package model.enums;
-
-public enum StatusCaixa {
-    ABERTO,
-    FECHADO
-}
